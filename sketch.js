8b3988cc0831486893fa77d0b12d7e5d5f5750a5
pritishprocoder <65237674+pritishprocoder@users.noreply.github.com>
2020-07-02 19:57:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var canvas, backgroundImage;
+var car1,car2,car3,car4;
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+var cars;
+var form, player, game;
+
+
+function setup(){
+  canvas = createCanvas(displayWidth-60,displayHeight-60);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+}
